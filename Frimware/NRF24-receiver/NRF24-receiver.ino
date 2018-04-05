89a2bd3cd7bf9023b9215095cb5993591c50ffe4
Kareem Elzeftawy <karem.elzftawy@gmail.com>
2018-04-05 22:23:45 +0200

    Add files via upload

---
+++ Frimware/NRF24-receiver/NRF24-receiver.ino
@@ -0,0 +1,46 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7,8);
+//RF24 radio(2,15);
+
+const int LED = 2; 
+const byte address[6] = "00001";
+
+void setup() {
+  // put your setup code here, to run once:
+  //ESP.wdtDisable();
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println(F("RF24/Simple Receive data Test"));
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+  radio.begin();
+  radio.openReadingPipe(0,address);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_250KBPS);
+  radio.startListening();
+}
+
+void loop() {
+  char text[32] = "";
+  
+  if (radio.available()) {
+    Serial.println("Received!");
+    radio.read(&text, sizeof(text));
+    String transData = String(text);
+    if (transData == "nrftest") {
+      Serial.print("Data received = ");
+      Serial.println(transData);
+        //tone(buzzer, 1000);
+        //delay(500);
+        //noTone(buzzer);
+        digitalWrite(LED, HIGH);
+        delay(500);
+        //digitalWrite(LED, LOW);
+      }    
+  }
+  else {
+  digitalWrite(LED, LOW);}
+}
---
+++ Frimware/NRF24-transmitter/NRF24-transmitter.ino
@@ -0,0 +1,35 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7,8);
+//RF24 radio(2,15);
+
+const byte address[6] = "00001";
+
+const int sensor = 2; 
+int state; // 0 close - 1 open Switch
+
+const char text[] = "nrftest";
+
+void setup() {
+  pinMode(sensor, INPUT_PULLUP);
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println(F("RF24/Simple Transmit data Test"));
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_250KBPS);
+  radio.stopListening();
+}
+
+void loop() {
+  state = digitalRead(sensor);
+  if (state == LOW)
+  {
+  radio.write(&text, sizeof(text));
+  Serial.println("Transmitted!");
+  delay(1000);  
+  }
+}
